1558ad05deabef41d54fd36776fa2ab29d589f2f
Praveen Yadav <pyadav9678@gmail.com>
2025-02-22 11:28:12 +0530

    initial commit

---
+++ components/overview.tsx
@@ -0,0 +1,27 @@
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+import { MessageIcon } from "./icons";
+import { LogoPython } from "@/app/icons";
+
+export const Overview = () => {
+  return (
+    <motion.div
+      key="overview"
+      className="max-w-3xl mx-auto md:mt-20"
+      initial={{ opacity: 0, scale: 0.98 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.98 }}
+      transition={{ delay: 0.5 }}
+    >
+      <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
+        <p className="flex flex-row justify-center gap-4 items-center text-lg font-semibold">
+          Last Minute legends
+        </p>
+        <p>
+          We are a team of three people formed in a last minute of hackathon.
+        </p>
+      </div>
+    </motion.div>
+  );
+};
